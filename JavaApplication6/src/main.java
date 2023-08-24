eb86199997163ff47912eed8b8556761a8105b39
ksalazars5 <89678542+ksalazars5@users.noreply.github.com>
2023-08-23 20:04:50 -0600

    Add files via upload

---
+++ JavaApplication6/src/main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author salaz
+ */
+public class main {
+     public static void main(String[] args) {  
+        
+        FRM_ManejodeCadenas frm = new FRM_ManejodeCadenas();
+        frm.setVisible(true);
+    }
+}
